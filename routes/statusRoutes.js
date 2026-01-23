9f8cb8e63061f05a7baec9588bad6c124ad2d4ed
Ismailov-M11 <ismailov.mukhriddin@gmail.com>
2026-01-23 23:24:43 +0500

    fix: resolve git merge conflicts in statusRoutes.js

--- routes/statusRoutes.js
+++ routes/statusRoutes.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const statusController = require("../controllers/statusController");
-<<<<<<< HEAD
-=======
 const userSettingsController = require("../controllers/userSettingsController");
->>>>>>> 7c8f7ba (feat: add user column settings API and database migration)
 
 router.get("/reports/new-pharmacies", statusController.getNewPharmaciesReport);
 // Temporary Debug Endpoint
@@ -15,11 +12,7 @@ router.patch("/update/:pharmacy_id", statusController.updateStatus);
 router.get("/history/:pharmacy_id", statusController.getStatusHistory);
 router.delete("/history/:id", statusController.deleteStatus);
 
-<<<<<<< HEAD
-=======
 // User Settings Routes
 router.get("/user-settings/:userId/column-settings", userSettingsController.getColumnSettings);
 router.post("/user-settings/:userId/column-settings", userSettingsController.saveColumnSettings);
-
->>>>>>> 7c8f7ba (feat: add user column settings API and database migration)
 module.exports = router;
